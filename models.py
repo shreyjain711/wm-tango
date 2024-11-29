03cda852b9f6cb3d6109d36ab53a2ac4e93888d8
Shrey Jain <31862368+shreyjain711@users.noreply.github.com>
2024-11-30 01:32:55 +0530

    adding another findme

--- models.py
+++ models.py
@@ -256,6 +256,10 @@ class AudioDiffusion(nn.Module):
             latents = self.group_out(latents.permute(0, 2, 3, 1).contiguous()).permute(0, 3, 1, 2).contiguous()
         return latents
 
+    def add_tr_watermark(latents):
+        print("FINDME", latents.shape)
+        return latents
+    
     def prepare_latents(self, batch_size, inference_scheduler, num_channels_latents, dtype, device):
         shape = (batch_size, num_channels_latents, 256, 16)
         latents = randn_tensor(shape, generator=None, device=device, dtype=dtype)
